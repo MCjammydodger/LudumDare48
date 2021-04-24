76ea2dfe6294e315a884d8c83f642b59a9271535
James Hookway <jimmer.hook@btinternet.com>
2021-04-24 09:10:16 +0100

    Level borders

---
+++ LudumDare48/Assets/Scripts/Block.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class Block : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        BoxCollider2D boxCollider2D = GetComponent<BoxCollider2D>();
+
+        boxCollider2D.size = spriteRenderer.size;
+    }
+}
---
+++ LudumDare48/Assets/Scripts/Level.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    [SerializeField] private float width = 80;
+    [SerializeField] private float height = 40;
+    [SerializeField] private SpriteRenderer leftBorder = null;
+    [SerializeField] private SpriteRenderer rightBorder = null;
+    [SerializeField] private SpriteRenderer topBorder = null;
+    [SerializeField] private SpriteRenderer bottomBorder = null;
+
+    private const float screenWidth = 40;
+    private const float screenHeight = 20;
+
+    private void Awake()
+    {
+        SetupBoundaries();
+    }
+
+    
+    private void SetupBoundaries()
+    {
+        float halfWidth = width / 2;
+        float halfHeight = height / 2;
+
+        leftBorder.size = new Vector2(screenWidth, height + 20);
+        rightBorder.size = new Vector3(screenWidth, height + 20);
+        topBorder.size = new Vector3(width + 40, screenHeight);
+        bottomBorder.size = new Vector3(width + 40, screenHeight);
+
+        leftBorder.transform.position = new Vector2(-halfWidth - (leftBorder.size.x / 2), halfHeight);
+        rightBorder.transform.position = new Vector2(halfWidth + (rightBorder.size.x / 2), halfHeight);
+        topBorder.transform.position = new Vector2(0, height + (topBorder.size.y / 2));
+        bottomBorder.transform.position = new Vector2(0, -bottomBorder.size.y / 2);
+    }
+}
